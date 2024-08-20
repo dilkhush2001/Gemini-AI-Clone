32fdeac97432d523159a615ef7afce00545f11ec
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-08-21 00:42:29 +0530

    deploy

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,7 @@ import {
   const MODEL_NAME = "gemini-1.0-pro";
   
   // Paste Your API KEY Below
-  const API_KEY = "Paste Your API KEY Here";
+  const API_KEY = "AIzaSyCmZXXz_ZuYX1iSiKE0W7O_DK0tgzWDmjU";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
